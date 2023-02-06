f0ac9ea24639d29a56d858eb398166d3a2bf0b83
DESKTOP-J4A1050\kryst <Krystofbumbalek2@gmail.com>
2023-02-06 12:57:05 +0100

    c

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { BookList } from "./pages/BookList";
 import { Home } from "./pages/Home";
+import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -18,7 +21,16 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/books" element={<BookList />} />
+        <Route path="/books">
+          <Route index element={<BookList />} /> // u defaultniho /books
+          nenastavuju path, nastavim index misto toho
+          <Route path=":id" element={<Book />} />
+          <Route path="new" element={<NewBook />} />
+        </Route>
+        {/* <Route path="/books" element={<BookList />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
